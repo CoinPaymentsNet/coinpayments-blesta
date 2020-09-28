f7ffb5375cb94ebfa8d5cccce6ac8f3aaf1a04fa
letscode <roman@ukrainiansoftware.com>
2020-09-28 22:46:51 +0300

    Unify the naming of configuration fields

--- language/en_us/coin_payments.php
+++ language/en_us/coin_payments.php
@@ -1,9 +1,9 @@
 <?php
 $lang['CoinPayments.name'] = "CoinPayments.net";
 
-$lang['CoinPayments.client_id'] = "CoinPayments Client ID";
-$lang['CoinPayments.webhooks'] = "CoinPayments Webhooks";
-$lang['CoinPayments.client_secret'] = "CoinPayments Client Secret";
+$lang['CoinPayments.client_id'] = "Client ID";
+$lang['CoinPayments.webhooks'] = "Webhooks";
+$lang['CoinPayments.client_secret'] = "Client Secret";
 $lang['CoinPayments.buildprocess.submit'] = "Pay with CoinPayments";
 $lang['CoinPayments.webhooks_enabled'] = 'Enabled';
 $lang['CoinPayments.webhooks_disabled'] = 'Disabled';
